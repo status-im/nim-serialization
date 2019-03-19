3a2595be942fe597801e5351bb15c65795991cd0
Zahary Karadjov <zahary@gmail.com>
2019-03-20 01:44:30 +0200

    Add a common interface for producing better error messages

--- serialization/errors.nim
+++ serialization/errors.nim
@@ -2,3 +2,6 @@ type
   SerializationError* = object of CatchableError
   UnexpectedEofError* = object of SerializationError
 
+method formatMsg*(err: ref SerializationError, filename: string): string {.base.} =
+  "Serialisation error while processing " & filename
+
