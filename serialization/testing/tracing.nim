897a6638cf4c168f498c946c166b01137cb4e4a2
Zahary Karadjov <zahary@gmail.com>
2019-08-02 11:52:28 +0300

    Extract common test cases and patterns used by both SSZ and JSON

---
+++ serialization/testing/tracing.nim
@@ -0,0 +1,33 @@
+when defined(serialization_tracing):
+  var tracingEnabled* = false
+
+  ## TODO
+  ## Implement a tracing context object that will be
+  ## able to track down which object fields are currently
+  ## entered. It will print the debug output in a form
+  ## that can be easily collapsed with indentation-based
+  ## outlining in most text editors.
+
+  func isTracingEnabled: bool =
+    # TODO this is a work-around for the lack of working
+    # `{.noSideEffect.}:` override in Nim 0.19.6.
+    {.emit: "`result` = `tracingEnabled`;".}
+
+  template traceSerialization*(args: varargs[untyped]) =
+    ## `traceSerialization` can be used to capture precise
+    ## traces of the serialization and deserialization of
+    ## complex formats.
+    if isTracingEnabled():
+      debugEcho args
+
+  template trs*(args: varargs[untyped]) =
+    ## `trs` is shorter form for "trace serialization"
+    ## that's easy to write during active development
+    ## and easy to replace with `traceSerialization`
+    ## once your library is complete :)
+    traceSerialization(args)
+
+else:
+  template traceSerialization*(args: varargs[untyped]) = discard
+  template trs*(args: varargs[untyped]) = discard
+
