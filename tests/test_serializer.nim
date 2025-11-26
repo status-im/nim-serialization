207216c6a0b4622fab9acf0a8ad6d6505f63b5cc
Esteban C Borsani <ecastroborsani@gmail.com>
2025-11-26 17:18:25 -0300

    Fix decode params forwarding (#113)

--- tests/test_serializer.nim
+++ tests/test_serializer.nim
@@ -81,3 +81,101 @@ suite "Rountrips":
     var val: ref string
     let ser = Ser.encode(val)
     check Ser.decode(ser, typeof(val)).isNil()
+
+type StringLimErr = object of SerializationError
+type StringLim = distinct string
+
+proc `==`(a, b: StringLim): bool {.borrow.}
+proc add(a: var StringLim, b: char) {.borrow.}
+
+proc readValue(r: var SerReader, val: var StringLim) {.raises: [IOError, SerializationError].} =
+  consumeKind r, SerKind.String
+  let L = r.readUint64()
+  if L > r.conf.limit.uint64:
+    raise newException(StringLimErr, "limit err")
+  for _ in 0 ..< L:
+    val.add r.stream.read().char
+
+suite "Config":
+  test "pass let conf":
+    let val = "1234567890"
+    let ser = Ser.encode(val)
+
+    let conf10 = SerConf(limit: 10)
+    check Ser.decode(ser, StringLim, conf = conf10) == val.StringLim
+    check Ser.decode(ser, StringLim, conf10) == val.StringLim
+
+    let conf5 = SerConf(limit: 5)
+    expect StringLimErr:
+      discard Ser.decode(ser, StringLim, conf = conf5)
+    expect StringLimErr:
+      discard Ser.decode(ser, StringLim, conf5)
+
+  test "pass const conf":
+    let val = "1234567890"
+    let ser = Ser.encode(val)
+
+    const conf10 = SerConf(limit: 10)
+    check Ser.decode(ser, StringLim, conf = conf10) == val.StringLim
+    check Ser.decode(ser, StringLim, conf10) == val.StringLim
+
+    const conf5 = SerConf(limit: 5)
+    expect StringLimErr:
+      discard Ser.decode(ser, StringLim, conf = conf5)
+    expect StringLimErr:
+      discard Ser.decode(ser, StringLim, conf5)
+
+  test "pass inlined conf":
+    let val = "1234567890"
+    let ser = Ser.encode(val)
+
+    check Ser.decode(ser, StringLim, conf = SerConf(limit: 10)) == val.StringLim
+    check Ser.decode(ser, StringLim, SerConf(limit: 10)) == val.StringLim
+
+    expect StringLimErr:
+      discard Ser.decode(ser, StringLim, conf = SerConf(limit: 5))
+    expect StringLimErr:
+      discard Ser.decode(ser, StringLim, SerConf(limit: 5))
+
+  test "pass expression conf":
+    let val = "1234567890"
+    let ser = Ser.encode(val)
+
+    template conf10: untyped =
+      var conf = SerConf(limit: 10)
+      conf
+
+    check Ser.decode(ser, StringLim, conf = conf10) == val.StringLim
+    check Ser.decode(ser, StringLim, conf10) == val.StringLim
+
+    template conf5: untyped =
+      var conf = SerConf(limit: 5)
+      conf
+
+    expect StringLimErr:
+      discard Ser.decode(ser, StringLim, conf = conf5)
+    expect StringLimErr:
+      discard Ser.decode(ser, StringLim, conf5)
+
+  test "multi params":
+    func init(
+        R: type SerReader,
+        stream: InputStream,
+        limit1: int,
+        limit2: int,
+        conf = default(SerConf)
+    ): R =
+      R(stream: stream, conf: SerConf(limit: conf.limit + limit1 + limit2))
+
+    let val = "1234567890"
+    let ser = Ser.encode(val)
+
+    let lim1 = 10
+    let lim2 = 0
+    check:
+      Ser.decode(ser, StringLim, 10, 0) == val.StringLim
+      Ser.decode(ser, StringLim, 0, 10) == val.StringLim
+      Ser.decode(ser, StringLim, 0, 0, SerConf(limit: 10)) == val.StringLim
+      Ser.decode(ser, StringLim, limit1 = 10, limit2 = 0) == val.StringLim
+      Ser.decode(ser, StringLim, limit1 = 0, limit2 = 10) == val.StringLim
+      Ser.decode(ser, StringLim, lim1, lim2) == val.StringLim
